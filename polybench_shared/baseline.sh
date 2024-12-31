37990669704a8815df1164f84c2122a235cf6b9e
jhsong27 <jaehyun.song@csi.skku.edu>
2024-12-31 03:03:17 +0000

    baseline modified

--- polybench_shared/baseline.sh
+++ polybench_shared/baseline.sh
@@ -1,3 +1,46 @@
 #!/bin/bash
 
-test
+HOME=`pwd`
+DIR1="PolyBench-ACC/CUDA/linear-algebra/kernels"
+DIR2="PolyBench-ACC/CUDA/stencils"
+KER="
+2mm
+3mm
+atax
+bicg
+doitgen
+gemm
+gemver
+gesummv
+mvt
+syr2k
+syrk
+"
+STN="
+adi
+2DConvolution
+3DConvolution
+fdtd2d
+jacobi1D
+jacobi2D
+"
+
+for bench in ${KER}
+do
+	cd $HOME/$DIR1
+
+	cd $bench
+
+	./${bench}.exe
+done
+
+for bench in ${STN}
+do
+	cd $HOME/$DIR2
+
+	cd $bench
+
+	./${bench}.exe
+done
+
+
