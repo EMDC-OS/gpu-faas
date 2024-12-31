07167f7857e87dc0d7a2e0c4c1f8c1ce4a0be0d0
jhsong27 <jaehyun.song@csi.skku.edu>
2024-12-31 04:47:56 +0000

    remove unnecessary directory

--- REWIND/microbench/sub.py
+++
@@ -1,8 +0,0 @@
-import os
-import subprocess
-import time
-
-time.sleep(1)
-p = subprocess.Popen(['/home/user01/gpu-faas/REWIND/microbench/exec'])
-print(time.time())
-(o, e) = p.communicate()
--- REWIND/microbench/test.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-
-startTime = Date.now()
-console.log(startTime)
-console.log("{ \"FUCK\":  }")
